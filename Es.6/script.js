9acc93ad407fc5b6303d3f93c50d58557c4d55c1
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-11-20 13:36:16 +0100

    m

--- Es.6/script.js
+++ Es.6/script.js
@@ -1,9 +1,10 @@
 const array = ["pesca", "ciliege", "maracuja"];
 let bottone = document.getElementById("bottone");
 let testo = document.getElementById("testo");
+let numero = document.getElementById("numero");
 
 function elementi(){
-    testo.textContent = "L'array contiene " + array.length + " elementi.";
+    console.log(array[Number(numero.value)])
 }
 
 bottone.addEventListener("click", elementi);
