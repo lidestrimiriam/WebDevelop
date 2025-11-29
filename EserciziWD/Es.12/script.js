b7fbad738810182630b089737016f94907e9fcf7
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-11-29 20:17:18 +0100

    l

--- EserciziWD/Es.12/script.js
+++ EserciziWD/Es.12/script.js
@@ -1,3 +1,29 @@
-let numero = document.getElementById("numero");
-let bottonePiù = document.getElementById("bottonePiù");
+let limite = document.getElementById("limite");
+let bottonePiu = document.getElementById("bottonePiu");
 let bottoneMeno = document.getElementById("bottoneMeno");
+let testo = document.getElementById("testo");
+let risultato = document.getElementById("risultato");
+
+let valore = 0;
+
+function incrementa (){
+  valore++;
+  risultato.textContent = valore;
+
+  if(valore >= limite.value){
+    testo.textContent = "Limite raggiunto!"
+  }
+}
+
+function decrementa(){
+  valore--;
+  risultato.textContent = valore;
+  if(valore <= 0){
+    testo.textContent = "Limite raggiunto!"
+  }
+}
+
+
+
+bottonePiu.addEventListener("click", incrementa);
+bottoneMeno.addEventListener("click", decrementa);
