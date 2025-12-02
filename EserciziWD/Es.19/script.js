d45d69539608724fa294ea044fb9838c1bc425ba
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-12-02 17:56:09 +0100

    q

---
+++ EserciziWD/Es.19/script.js
@@ -0,0 +1,12 @@
+let testo = document.getElementById("testo");
+let bottone = document.getElementById("bottone");
+let risultato = document.getElementById("risultato");
+
+let contatore = 0;
+bottone.disabled = true;
+function conta(){
+
+}
+
+
+bottone.addEventListener("click", conta);
