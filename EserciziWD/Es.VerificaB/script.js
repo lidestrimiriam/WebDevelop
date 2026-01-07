6bd834020d53d73fcbacf583a36dc0b53d98d6d7
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-01-07 21:01:45 +0100

    a

--- EserciziWD/Es.VerificaB/script.js
+++ EserciziWD/Es.VerificaB/script.js
@@ -1,31 +1,83 @@
 let codicePC = document.getElementById("codicePC");
 let utente = document.getElementById("utente");
 let Boccupa = document.getElementById("Boccupa");
-let Bolibera = document.getElementById("Blibera");
+let Blibera = document.getElementById("Blibera");
 let Belimina = document.getElementById("Belimina");
-let div = document.getElementById("aula");
+let area = document.getElementById("area");
+let messaggio = document.getElementById("messaggio");
 
-const PC = [
+let computers = [
     {
       codicePC: "A01",
-      utente: "Miriam",
+      utente: "",
       stato: "libero"
     },
     {
       codicePC: "A02",
-      utente: "Diego",
+      utente: "",
       stato: "occupato"
     },
     {
       codicePC: "A03",
-      utente: "Stellina",
+      utente: "",
       stato: "libero"
     }
   ];
 
 function visualizzaAula(){
+  area.textContent = "";
+  for(let computer of computers){
+    let div = document.createElement("div");
+    div.textContent = computer.codicePC +" "+ computer.utente +" "+ computer.stato;
+    area.append(div);
+  }
+}
 
+visualizzaAula();
 
+function occupaPC(){
+  for(let computer of computers){
+    if(codicePC.value === computer.codicePC){
+      computer.stato = "occupato";
+      computer.utente = utente.value;
+      }
+  }
+  visualizzaAula();
+}
+
+function liberaPC(){
+  for(let computer of computers){
+    if(codicePC.value === computer.codicePC){
+      computer.stato = "libero";
+      computer.utente = "";
+      }
+  }
+  visualizzaAula();
+}
+
+function cancellaPC(){
+  let array = [];
+  for(let computer of computers){
+    if(codicePC.value !== computer.codicePC){
+      array.push(computer);
+    }
+  }
+  computers = array;
+  visualizzaAula()
 }
 
 
+
+
+codicePC.addEventListener("input", () => {
+  let codice = codicePC.value;
+  if(codice.length === 3){
+    messaggio.textContent = "";
+  }else{
+    messaggio.textContent = "Il codice non va bene";
+  }
+})
+
+Blibera.addEventListener("click", liberaPC);
+Boccupa.addEventListener("click", occupaPC);
+Belimina.addEventListener("click", cancellaPC);
