d3d17479efc9b017baf03992ceaa0e79f194ce09
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-01-07 17:20:36 +0100

    n

--- EserciziWD/Es.VerificaB/script.js
+++ EserciziWD/Es.VerificaB/script.js
@@ -3,3 +3,29 @@ let utente = document.getElementById("utente");
 let Boccupa = document.getElementById("Boccupa");
 let Bolibera = document.getElementById("Blibera");
 let Belimina = document.getElementById("Belimina");
+let div = document.getElementById("aula");
+
+const PC = [
+    {
+      codicePC: "A01",
+      utente: "Miriam",
+      stato: "libero"
+    },
+    {
+      codicePC: "A02",
+      utente: "Diego",
+      stato: "occupato"
+    },
+    {
+      codicePC: "A03",
+      utente: "Stellina",
+      stato: "libero"
+    }
+  ];
+
+function visualizzaAula(){
+
+
+}
+
+
