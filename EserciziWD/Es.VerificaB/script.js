93ffca72ad8ae3f422b5117417b597a3b2f30976
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-01-07 16:32:40 +0100

    Update script.js

---
+++ EserciziWD/Es.VerificaB/script.js
@@ -0,0 +1,5 @@
+let codicePC = document.getElementById("codicePC");
+let utente = document.getElementById("utente");
+let Boccupa = document.getElementById("Boccupa");
+let Bolibera = document.getElementById("Blibera");
+let Belimina = document.getElementById("Belimina");
